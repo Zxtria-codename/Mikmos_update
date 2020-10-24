d09175eea721c39656d8e7967e2b55faa1224b4d
sajakaprata <37566903+sajakaprata@users.noreply.github.com>
2020-10-24 22:26:23 +0700

    Add files via upload

---
+++ assets/js/lib/datatables/datatables-init.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('#mikmos-tbl-noinfo').DataTable( {
+        "order": [[ 1, "asc" ]]
+    } );
+} );
+$(document).ready(function() {
+    $('#mikmos-tbl-noinfo1').DataTable( {
+        "order": [[ 0, "asc" ]]
+    } );
+} );
+$(document).ready(function() {
+    $('#mikmos-tbl-desc').DataTable( {
+        "order": [[ 1, "desc" ]]
+    } );
+} );
+$(document).ready(function() {
+    $('#mikmos-tbl-desc1').DataTable( {
+        "order": [[ 0, "desc" ]]
+    } );
+} );
